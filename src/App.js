40bd4eda4a141d2ef9146dc2b93a2bbc0671d8a0
ckelley_17 <ckelleycek@gmail.com>
2025-10-15 19:36:04 -0400

    made react setup, implemented simple background with homepage banner template

---
+++ src/App.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    //LANDING PAGE
+    <div className="App"> 
+      <div style={{ backgroundColor: "white", minHeight: "50vh", padding: "10vh", color: "#2F4432"}}>
+      <h1 style={{}}>TEAM B
+      </h1>
+
+  <div style={{backgroundColor: "#2F4432", minHeight: "5vh", padding: "10vh"}}>
+    <h2 style={{color: "white"}}> Bridging the gap between innovation and comprehension on Artificial Intellegence </h2>
+
+  </div>
+  </div>
+  </div>
+    
+  );
+}
+
+export default App;
